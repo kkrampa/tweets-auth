a37940193fb27efb3446f5651e564d47dc4679a1
Kamil Krampa <kkrampa@soldevelo.com>
2016-10-14 19:37:26 +0200

    Added docker settings

---
+++ tweets_auth/docker_settings.py
@@ -0,0 +1,14 @@
+try:
+    from .settings import *
+except ImportError:
+    pass
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
